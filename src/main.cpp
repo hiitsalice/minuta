b820754c0d7b3a6b868d8094f3da5182ca95d5dd
Alice Brooks <thisisalicebrooks@gmail.com>
2026-08-29 19:52:06 +0000

    Use DM Sans as reading font

--- src/main.cpp
+++ src/main.cpp
@@ -86,28 +86,28 @@ EpdFont notoserif18BoldItalicFont(&youngserif_18_bolditalic);
 EpdFontFamily notoserif18FontFamily(&notoserif18RegularFont, &notoserif18BoldFont, &notoserif18ItalicFont,
                                     &notoserif18BoldItalicFont);
 
-EpdFont notosans12RegularFont(&notosans_12_regular);
-EpdFont notosans12BoldFont(&notosans_12_bold);
-EpdFont notosans12ItalicFont(&notosans_12_italic);
-EpdFont notosans12BoldItalicFont(&notosans_12_bolditalic);
+EpdFont notosans12RegularFont(&dmsans_12_regular);
+EpdFont notosans12BoldFont(&dmsans_12_bold);
+EpdFont notosans12ItalicFont(&dmsans_12_italic);
+EpdFont notosans12BoldItalicFont(&dmsans_12_bolditalic);
 EpdFontFamily notosans12FontFamily(&notosans12RegularFont, &notosans12BoldFont, &notosans12ItalicFont,
                                    &notosans12BoldItalicFont);
-EpdFont notosans14RegularFont(&notosans_14_regular);
-EpdFont notosans14BoldFont(&notosans_14_bold);
-EpdFont notosans14ItalicFont(&notosans_14_italic);
-EpdFont notosans14BoldItalicFont(&notosans_14_bolditalic);
+EpdFont notosans14RegularFont(&dmsans_14_regular);
+EpdFont notosans14BoldFont(&dmsans_14_bold);
+EpdFont notosans14ItalicFont(&dmsans_14_italic);
+EpdFont notosans14BoldItalicFont(&dmsans_14_bolditalic);
 EpdFontFamily notosans14FontFamily(&notosans14RegularFont, &notosans14BoldFont, &notosans14ItalicFont,
                                    &notosans14BoldItalicFont);
-EpdFont notosans16RegularFont(&notosans_16_regular);
-EpdFont notosans16BoldFont(&notosans_16_bold);
-EpdFont notosans16ItalicFont(&notosans_16_italic);
-EpdFont notosans16BoldItalicFont(&notosans_16_bolditalic);
+EpdFont notosans16RegularFont(&dmsans_16_regular);
+EpdFont notosans16BoldFont(&dmsans_16_bold);
+EpdFont notosans16ItalicFont(&dmsans_16_italic);
+EpdFont notosans16BoldItalicFont(&dmsans_16_bolditalic);
 EpdFontFamily notosans16FontFamily(&notosans16RegularFont, &notosans16BoldFont, &notosans16ItalicFont,
                                    &notosans16BoldItalicFont);
-EpdFont notosans18RegularFont(&notosans_18_regular);
-EpdFont notosans18BoldFont(&notosans_18_bold);
-EpdFont notosans18ItalicFont(&notosans_18_italic);
-EpdFont notosans18BoldItalicFont(&notosans_18_bolditalic);
+EpdFont notosans18RegularFont(&dmsans_18_regular);
+EpdFont notosans18BoldFont(&dmsans_18_bold);
+EpdFont notosans18ItalicFont(&dmsans_18_italic);
+EpdFont notosans18BoldItalicFont(&dmsans_18_bolditalic);
 EpdFontFamily notosans18FontFamily(&notosans18RegularFont, &notosans18BoldFont, &notosans18ItalicFont,
                                    &notosans18BoldItalicFont);
 
