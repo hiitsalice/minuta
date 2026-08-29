e1cc955d319d197bf98de6697cfb0776ce5d86e2
Alice Brooks <thisisalicebrooks@gmail.com>
2026-08-29 22:12:26 +0700

    Update CrossPointSettings.h
    Add Solum and Quartum, make Solum default

--- src/CrossPointSettings.h
+++ src/CrossPointSettings.h
@@ -166,7 +166,7 @@ class CrossPointSettings : public PersistableStore<CrossPointSettings> {
   };
 
   // UI Theme
-  enum UI_THEME { CLASSIC = 0, LYRA = 1, LYRA_3_COVERS = 2, ROUNDEDRAFF = 3 };
+  enum UI_THEME { SOLUM = 0, QUARTUM = 1, CLASSIC = 2, LYRA = 3, LYRA_3_COVERS = 4, ROUNDEDRAFF = 5 };
 
   // Image rendering in EPUB reader
   enum IMAGE_RENDERING { IMAGES_DISPLAY = 0, IMAGES_PLACEHOLDER = 1, IMAGES_SUPPRESS = 2, IMAGE_RENDERING_COUNT };
