5c77701c68c0dd86a4363582dcd5bd6eac81ca9f
Alice Brooks <thisisalicebrooks@gmail.com>
2026-08-29 17:48:46 +0000

    Use Young Serif as reading font

--- src/main.cpp
+++ src/main.cpp
@@ -60,29 +60,29 @@ constexpr unsigned long X4PRO_POWER_CLICK_MAX_HOLD_MS = 300;
 static bool wakePowerReleasePending = false;
 
 // Fonts
-EpdFont notoserif14RegularFont(&notoserif_14_regular);
-EpdFont notoserif14BoldFont(&notoserif_14_bold);
-EpdFont notoserif14ItalicFont(&notoserif_14_italic);
-EpdFont notoserif14BoldItalicFont(&notoserif_14_bolditalic);
+EpdFont notoserif14RegularFont(&youngserif_14_regular);
+EpdFont notoserif14BoldFont(&youngserif_14_bold);
+EpdFont notoserif14ItalicFont(&youngserif_14_italic);
+EpdFont notoserif14BoldItalicFont(&youngserif_14_bolditalic);
 EpdFontFamily notoserif14FontFamily(&notoserif14RegularFont, &notoserif14BoldFont, &notoserif14ItalicFont,
                                     &notoserif14BoldItalicFont);
 #ifndef OMIT_FONTS
-EpdFont notoserif12RegularFont(&notoserif_12_regular);
-EpdFont notoserif12BoldFont(&notoserif_12_bold);
-EpdFont notoserif12ItalicFont(&notoserif_12_italic);
-EpdFont notoserif12BoldItalicFont(&notoserif_12_bolditalic);
+EpdFont notoserif12RegularFont(&youngserif_12_regular);
+EpdFont notoserif12BoldFont(&youngserif_12_bold);
+EpdFont notoserif12ItalicFont(&youngserif_12_italic);
+EpdFont notoserif12BoldItalicFont(&youngserif_12_bolditalic);
 EpdFontFamily notoserif12FontFamily(&notoserif12RegularFont, &notoserif12BoldFont, &notoserif12ItalicFont,
                                     &notoserif12BoldItalicFont);
-EpdFont notoserif16RegularFont(&notoserif_16_regular);
-EpdFont notoserif16BoldFont(&notoserif_16_bold);
-EpdFont notoserif16ItalicFont(&notoserif_16_italic);
-EpdFont notoserif16BoldItalicFont(&notoserif_16_bolditalic);
+EpdFont notoserif16RegularFont(&youngserif_16_regular);
+EpdFont notoserif16BoldFont(&youngserif_16_bold);
+EpdFont notoserif16ItalicFont(&youngserif_16_italic);
+EpdFont notoserif16BoldItalicFont(&youngserif_16_bolditalic);
 EpdFontFamily notoserif16FontFamily(&notoserif16RegularFont, &notoserif16BoldFont, &notoserif16ItalicFont,
                                     &notoserif16BoldItalicFont);
-EpdFont notoserif18RegularFont(&notoserif_18_regular);
-EpdFont notoserif18BoldFont(&notoserif_18_bold);
-EpdFont notoserif18ItalicFont(&notoserif_18_italic);
-EpdFont notoserif18BoldItalicFont(&notoserif_18_bolditalic);
+EpdFont notoserif18RegularFont(&youngserif_18_regular);
+EpdFont notoserif18BoldFont(&youngserif_18_bold);
+EpdFont notoserif18ItalicFont(&youngserif_18_italic);
+EpdFont notoserif18BoldItalicFont(&youngserif_18_bolditalic);
 EpdFontFamily notoserif18FontFamily(&notoserif18RegularFont, &notoserif18BoldFont, &notoserif18ItalicFont,
                                     &notoserif18BoldItalicFont);
 
