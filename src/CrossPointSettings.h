7e104356b8c16a5e7988b5182d2d031a6a19ccdf
Alice Brooks <thisisalicebrooks@gmail.com>
2026-08-29 22:13:21 +0700

    Update CrossPointSettings.h
    Make Solum the default theme

--- src/CrossPointSettings.h
+++ src/CrossPointSettings.h
@@ -277,7 +277,7 @@ class CrossPointSettings : public PersistableStore<CrossPointSettings> {
   // Defaults to Disabled so shortcut-based bookmark toggling remains opt-in.
   uint8_t longPressMenuFunction = LP_MENU_DISABLED;
   // UI Theme
-  uint8_t uiTheme = LYRA;
+  uint8_t uiTheme = SOLUM;
   // Sunlight fading compensation
   uint8_t fadingFix = 0;
   // Power button return from footnotes (1 = enabled, 0 = disabled)
