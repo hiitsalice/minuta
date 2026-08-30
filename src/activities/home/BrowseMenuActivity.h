102bb136e651ffb4917809e44e60fdc6b0230142
Alice Brooks <thisisalicebrooks@gmail.com>
2026-08-30 19:49:51 +0700

    Create BrowseMenuActivity.h
    Add Browse menu header

---
+++ src/activities/home/BrowseMenuActivity.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "activities/UiListActivity.h"
+
+// Simple 2-item menu opened from Solum/Quartum's "Browse" button:
+// Library (file browser) and Settings.
+class BrowseMenuActivity final : public UiListActivity {
+ public:
+  explicit BrowseMenuActivity(GfxRenderer& renderer, MappedInputManager& mappedInput);
+
+  static constexpr int MENU_ITEM_COUNT = 2;
+
+ private:
+  int listCount() const override;
+  void buildScreen(UiScreen& screen) override;
+  void activateIndex(int index) override;
+  const char* headerTitle() const override;
+
+  freeink::ui::ListItem rowItems_[MENU_ITEM_COUNT]{};
+};
