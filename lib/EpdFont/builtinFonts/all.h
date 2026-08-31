266adfa50444df8b5c87c8b6cdb7c00b50318781
Alice Brooks <thisisalicebrooks@gmail.com>
2026-09-01 00:25:57 +0700

    Update all.h
    Add Steinem font headers

--- lib/EpdFont/builtinFonts/all.h
+++ lib/EpdFont/builtinFonts/all.h
@@ -4,36 +4,63 @@
 #include <builtinFonts/youngserif_12_bolditalic.h>
 #include <builtinFonts/youngserif_12_italic.h>
 #include <builtinFonts/youngserif_12_regular.h>
+
 #include <builtinFonts/youngserif_14_bold.h>
 #include <builtinFonts/youngserif_14_bolditalic.h>
 #include <builtinFonts/youngserif_14_italic.h>
 #include <builtinFonts/youngserif_14_regular.h>
+
 #include <builtinFonts/youngserif_16_bold.h>
 #include <builtinFonts/youngserif_16_bolditalic.h>
 #include <builtinFonts/youngserif_16_italic.h>
 #include <builtinFonts/youngserif_16_regular.h>
+
 #include <builtinFonts/youngserif_18_bold.h>
 #include <builtinFonts/youngserif_18_bolditalic.h>
 #include <builtinFonts/youngserif_18_italic.h>
 #include <builtinFonts/youngserif_18_regular.h>
-#include <builtinFonts/notosans_8_regular.h>
+
 #include <builtinFonts/dmsans_12_bold.h>
 #include <builtinFonts/dmsans_12_bolditalic.h>
 #include <builtinFonts/dmsans_12_italic.h>
 #include <builtinFonts/dmsans_12_regular.h>
+
 #include <builtinFonts/dmsans_14_bold.h>
 #include <builtinFonts/dmsans_14_bolditalic.h>
 #include <builtinFonts/dmsans_14_italic.h>
 #include <builtinFonts/dmsans_14_regular.h>
+
 #include <builtinFonts/dmsans_16_bold.h>
 #include <builtinFonts/dmsans_16_bolditalic.h>
 #include <builtinFonts/dmsans_16_italic.h>
 #include <builtinFonts/dmsans_16_regular.h>
+
 #include <builtinFonts/dmsans_18_bold.h>
 #include <builtinFonts/dmsans_18_bolditalic.h>
 #include <builtinFonts/dmsans_18_italic.h>
 #include <builtinFonts/dmsans_18_regular.h>
-#include <builtinFonts/ubuntu_10_bold.h>
-#include <builtinFonts/ubuntu_10_medium.h>
-#include <builtinFonts/ubuntu_12_bold.h>
-#include <builtinFonts/ubuntu_12_medium.h>
+
+#include <builtinFonts/steinem_8_regular.h>
+#include <builtinFonts/steinem_8_italic.h>
+#include <builtinFonts/steinem_8_bold.h>
+#include <builtinFonts/steinem_8_bolditalic.h>
+
+#include <builtinFonts/steinem_10_regular.h>
+#include <builtinFonts/steinem_10_italic.h>
+#include <builtinFonts/steinem_10_bold.h>
+#include <builtinFonts/steinem_10_bolditalic.h>
+
+#include <builtinFonts/steinem_12_regular.h>
+#include <builtinFonts/steinem_12_italic.h>
+#include <builtinFonts/steinem_12_bold.h>
+#include <builtinFonts/steinem_12_bolditalic.h>
+
+#include <builtinFonts/steinem_21_regular.h>
+#include <builtinFonts/steinem_21_italic.h>
+#include <builtinFonts/steinem_21_bold.h>
+#include <builtinFonts/steinem_21_bolditalic.h>
+
+#include <builtinFonts/steinem_32_regular.h>
+#include <builtinFonts/steinem_32_italic.h>
+#include <builtinFonts/steinem_32_bold.h>
+#include <builtinFonts/steinem_32_bolditalic.h>
