83620e7a30ffd86128ba1590654ef153f4ad6417
Alice <YOUR_GITHUB_EMAIL>
2026-09-23 02:47:20 +0000

    Refresh frequency: cycle in place instead of popup

--- src/SettingsList.h
+++ src/SettingsList.h
@@ -223,7 +223,8 @@ inline std::vector<SettingInfo> getSettingsList(const SdCardFontRegistry* regist
         SettingInfo::Enum(StrId::STR_REFRESH_FREQ, &CrossPointSettings::refreshFrequency,
                           {StrId::STR_PAGES_1, StrId::STR_PAGES_5, StrId::STR_PAGES_10, StrId::STR_PAGES_15,
                            StrId::STR_PAGES_30, StrId::STR_NEVER},
-                          "refreshFrequency", StrId::STR_CAT_DISPLAY),
+                          "refreshFrequency", StrId::STR_CAT_DISPLAY)
+            .withNoPopup(),
         SettingInfo::Enum(StrId::STR_UI_THEME, &CrossPointSettings::uiTheme,
                           {StrId::STR_THEME_SOLUM, StrId::STR_THEME_QUARTUM}, "uiTheme", StrId::STR_CAT_DISPLAY),
         SettingInfo::Toggle(StrId::STR_SUNLIGHT_FADING_FIX, &CrossPointSettings::fadingFix, "fadingFix",
