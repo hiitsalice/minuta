5c6953a44530bdee461aefaec9f8147a7aa1632e
Alice <YOUR_GITHUB_EMAIL>
2026-09-23 02:45:14 +0000

    Power button click: cycle in place instead of popup

--- src/SettingsList.h
+++ src/SettingsList.h
@@ -293,7 +293,8 @@ inline std::vector<SettingInfo> getSettingsList(const SdCardFontRegistry* regist
         SettingInfo::Enum(
             StrId::STR_SHORT_PWR_BTN, &CrossPointSettings::shortPwrBtn,
             {StrId::STR_SLEEP, StrId::STR_PAGE_TURN, StrId::STR_FORCE_REFRESH, StrId::STR_FOOTNOTES},
-            "shortPwrBtn", StrId::STR_CAT_CONTROLS),
+            "shortPwrBtn", StrId::STR_CAT_CONTROLS)
+            .withNoPopup(),
         SettingInfo::Toggle(StrId::STR_PWR_BTN_FOOTNOTE_BACK, &CrossPointSettings::pwrBtnFootnoteBack,
                             "pwrBtnFootnoteBack", StrId::STR_CAT_CONTROLS),
         // --- System ---
